bca788b63f61ed89e499729d22a21e2667750117
Glenn Dwiyatcita <dwiyatci@mail.com>
2016-03-18 20:24:17 +0100

    Screw carets and favour Babel stage-2 preset

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = {
         ],
         loader : 'babel',
         query  : {
-          presets: ['es2015', 'stage-0', 'react'],
+          presets: ['es2015', 'stage-2', 'react'],
           plugins: ['transform-runtime'],
         },
       },
